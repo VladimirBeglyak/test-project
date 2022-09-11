92599132911a93835458cc7b25ca46545f5c6ce7
VladimirBeglyak <vladimir.beglyak91@gmail.com>
2022-09-11 21:09:21 +0300

    fix(test-project): add files from previous commit

--- src/main/java/com/vbdev/controller/LoginController.java
+++ src/main/java/com/vbdev/controller/LoginController.java
@@ -1,2 +1,31 @@
-package com.vbdev.controller;public class LoginController {
+package com.vbdev.controller;
+
+import com.vbdev.domain.UserAccount;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+  public static final Logger log = LogManager.getLogger();
+  private static final String USER_NAME = "vladimir.beglyak@mail.ru";
+  private static final String PASSWORD = "11111";
+  private static final String BAD_CREDENTIAL_MESSAGE = "Bad credential: email %s";
+
+  @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<UserAccount> login(@RequestBody UserAccount userAccount) {
+    log.info("Try to login UserAccount: {}", userAccount);
+    if (userAccount.getEmail().equals(USER_NAME) && userAccount.getPassword().equals(PASSWORD)) {
+      return ResponseEntity.ok(new UserAccount(userAccount.getEmail(), userAccount.getPassword()));
+    } else {
+      throw new RuntimeException(String.format(BAD_CREDENTIAL_MESSAGE, userAccount.getEmail()));
+    }
+  }
 }
--- src/main/java/com/vbdev/exception/RestExceptionHandler.java
+++ src/main/java/com/vbdev/exception/RestExceptionHandler.java
@@ -1,2 +1,18 @@
-package com.vbdev.exception;public class ExceptionHandler {
+package com.vbdev.exception;
+
+import java.time.Instant;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<ErrorResponse> handle(RuntimeException exception, HttpServletRequest request) {
+    ErrorResponse response = ErrorResponse.of(Instant.now(), exception.getMessage(), HttpStatus.UNAUTHORIZED.value(), request.getRequestURI());
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+  }
 }
